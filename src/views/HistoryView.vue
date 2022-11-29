2ebaf3231dc75fd46f3c1d36e4bdb5f0765faa87
temp <astrid.oras@gmail.com>
2022-11-29 09:07:47 +0200

    HistoryView

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,4 +1,29 @@
 <template>
+  <div class="row justify-content-center">
+  <div class="col col-lg-9 ">
+    <table class="table table-hover table-success">
+      <thead>
+      <tr>
+        <th scope="col">Pilt</th>
+        <th scope="col">Liik</th>
+        <th scope="col">Kõrgusvahemik</th>
+        <th scope="col">Piirkond</th>
+        <th scope="col">Hind</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th>Foto</th>
+        <td>Kuusk</td>
+        <td>0-1</td>
+        <td>Harjumaa</td>
+        <td>50€</td>
+      </tr>
+      </tbody>
+    </table>
+    </div>
+  </div>
+
 
 </template>
 
